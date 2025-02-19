a4d69294b987366c2ff401097c44635b62bb03eb
Colson Arthur <arthur.colson@etu.iut-tlse3.fr>
2025-02-19 17:36:10 +0100

    ça marche ?? c beau uwu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+    fetchMessages();
+    setInterval(fetchMessages, 2000); // Update messages every 2 seconds
+};
+
 function getUsername(){
     return $('#pseudo').val();
 }
@@ -25,8 +30,33 @@ function handleMessage(){
                 }
             };
             requete.send("auteur=" + encodeURIComponent(auteur) + "&contenu=" + encodeURIComponent(contenu));
+            fetchMessages();
             inputMessage.val('');
         }
+    });
+}
+
+function fetchMessages() {
+    var requete = new XMLHttpRequest();
+    requete.open("GET", "./php/recuperer.php", true);
+    requete.onreadystatechange = function() {
+        if (requete.readyState === 4 && requete.status === 200) {
+            var messages = JSON.parse(requete.responseText);
+            displayMessages(messages);
+        }
+    };
+    requete.send();
+}
+
+function displayMessages(messages) {
+    var chatDiv = $("#chat");
+    chatDiv.empty(); // Clear previous messages
+    messages.reverse().forEach(function(message) {
+        var messageElement = $("<div>").addClass("message");
+        messageElement.append($("<p>").html(`${message.auteur}`));
+        messageElement.append($("<p>").text(message.contenu));
+        messageElement.append($("<p>").html(`${message.horaire}`));
+        chatDiv.append(messageElement);
     });
 }
 
--- php/recuperer.php
+++ php/recuperer.php
@@ -3,8 +3,9 @@ $host = "localhost";
 $dbname = "messagerier4a10";
 $username = "root";
 $password = "";
+
 try {
-    $linkpdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
